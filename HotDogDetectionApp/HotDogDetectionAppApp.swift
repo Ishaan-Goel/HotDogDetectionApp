4ef30f7c172de0fe817cc8e5d2597bdbe1e561f7
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 13:08:11 -0800

    Initial Commit

---
+++ HotDogDetectionApp/HotDogDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotDogDetectionAppApp.swift
+//  HotDogDetectionApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HotDogDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
